2836041fe17419c4884a06a239ce306f391678a8
kienphan91 <97894384+kienphan91@users.noreply.github.com>
2022-10-09 20:05:03 +0700

    update

---
+++ src/routes/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img className='w-6 h-6' src={"../images/img-1.jpg"} alt="" />
+        <h1>Narbarasaw</h1>
+    </div>
+  )
+}
+
+export default Navbar
